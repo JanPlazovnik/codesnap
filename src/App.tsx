8bae61d7e9deac0bb365c4a3865320991feb6fef
Jan Plazovnik <jan.plazovnik@gmail.com>
2023-01-21 21:00:53 +0100

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+    return (
+        <div className="">
+            <div className="flex flex-col items-center justify-center min-h-screen py-2">
+                <div className="flex flex-col items-center justify-center w-full max-w-md bg-white rounded-lg shadow-xl">
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default App

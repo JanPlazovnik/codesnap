f1ecb2c4a965036d021b505bf78284d8a22826fc
Jan Plazovnik <jan.plazovnik@gmail.com>
2023-01-22 00:54:54 +0100

    feat: make wrapper relative and include settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import TextArea from './componets/TextArea';
+import Settings from './componets/Settings';
 function App() {
-    return (
-        <div className="grid min-h-screen place-items-center py-5">
-            <TextArea />
-        </div>
-    )
+	return (
+		<div className="grid min-h-screen place-items-center py-5 relative">
+			<TextArea />
+			<Settings />
+		</div>
+	);
 }
 
-export default App
+export default App;

daad1d7c4424eebf608528618d896a4482ced525
Jan Plazovnik <jan.plazovnik@gmail.com>
2023-01-22 00:38:11 +0100

    refactor: updated styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
+import TextArea from './componets/TextArea';
 function App() {
     return (
-        <div className="">
-            <div className="flex flex-col items-center justify-center min-h-screen py-2">
-                <div className="flex flex-col items-center justify-center w-full max-w-md bg-white rounded-lg shadow-xl">
-                </div>
-            </div>
+        <div className="grid min-h-screen place-items-center py-5">
+            <TextArea />
         </div>
     )
 }

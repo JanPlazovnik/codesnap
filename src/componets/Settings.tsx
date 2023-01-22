deb211723bb92ac7fcd6e13bbf1cae6021b04ee8
Jan Plazovnik <jan.plazovnik@gmail.com>
2023-01-22 01:12:09 +0100

    feat: added padding selector

--- src/componets/Settings.tsx
+++ src/componets/Settings.tsx
@@ -1,11 +1,16 @@
 import hljs from 'highlight.js';
-import { useSettingsState, LanguageSetting } from '../store/zustand';
+import {
+	useSettingsState,
+	LanguageSetting,
+	paddingOptions,
+	PaddingSetting,
+} from '../store/zustand';
 
 export default function Settings() {
 	const settings = useSettingsState();
 
 	return (
-		<div className="flex flex-col gap-2 fixed bottom-10 p-4 border-2 rounded-lg border-solid">
+		<div className="flex flex-row gap-5 fixed bottom-10 p-4 border-2 rounded-lg border-solid">
 			<div className="flex gap-2">
 				<label htmlFor="language">Language</label>
 				<select
@@ -18,6 +23,23 @@ export default function Settings() {
 					{hljs.listLanguages().map((language: string) => (
 						<option key={language} value={language}>
 							{language}
+						</option>
+					))}
+				</select>
+			</div>
+
+			<div className="flex gap-2">
+				<label htmlFor="padding">Padding</label>
+				<select
+					id="padding"
+					value={settings.padding}
+					onChange={(e) =>
+						settings.setPadding(parseInt(e.target.value) as PaddingSetting)
+					}
+				>
+					{paddingOptions.map((padding: number) => (
+						<option key={padding} value={padding}>
+							{padding}px
 						</option>
 					))}
 				</select>

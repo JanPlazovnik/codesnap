00fcc0dd31e145c0825421f455aaebc450c1d753
Jan Plazovnik <jan.plazovnik@gmail.com>
2023-01-23 15:29:51 +0100

    chore: clean up tailwind classes

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -35,14 +35,16 @@ const Settings = React.forwardRef((_, ref: any) => {
 	}, [ref]);
 
 	return (
-		<div className="flex flex-row gap-4 items-center fixed bottom-20 bg-neutral-300 dark:bg-neutral-900/70 p-2 rounded-lg z-10">
+		<div className="settings">
 			<div className="flex flex-row gap-1">
 				{gradients.map((gradient: string, index: number) => (
 					<div
 						key={index}
 						className="w-5 h-5 rounded-full bg-white cursor-pointer border-2 border-neutral-500"
 						style={{ background: gradient }}
-						onClick={() => settings.setGradient(gradient as GradientSetting)}
+						onClick={() =>
+							settings.setGradient(gradient as GradientSetting)
+						}
 					/>
 				))}
 			</div>
@@ -50,13 +52,17 @@ const Settings = React.forwardRef((_, ref: any) => {
 			<Select
 				value={settings.padding}
 				options={paddingOptions as unknown as string[]}
-				onChange={(option: string) => settings.setPadding(option as PaddingSetting)}
+				onChange={(option: string) =>
+					settings.setPadding(option as PaddingSetting)
+				}
 			/>
 
 			<Select
 				value={settings.language}
 				options={['auto', 'none', ...hljs.listLanguages()]}
-				onChange={(option: string) => settings.setLanguage(option as LanguageSetting)}
+				onChange={(option: string) =>
+					settings.setLanguage(option as LanguageSetting)
+				}
 			/>
 
 			<Button onClick={takeScreenshot}>

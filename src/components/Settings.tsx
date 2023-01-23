710183b177903206a64bb571fad5b2523ec117b9
Jan Plazovnik <jan.plazovnik@gmail.com>
2023-01-23 15:13:30 +0100

    chore: clean up settings panel

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -35,41 +35,33 @@ const Settings = React.forwardRef((_, ref: any) => {
 	}, [ref]);
 
 	return (
-		<div className="flex flex-row gap-4 fixed bottom-20 bg-neutral-300 dark:bg-neutral-900/70 p-2 rounded-lg z-10">
-			<div className="flex flex-col gap-1">
-				<div className="flex flex-row items-center justify-center h-full gap-1">
-					{gradients.map((gradient: string, index: number) => (
-						<div
-							key={index}
-							className="w-5 h-5 rounded-full bg-white cursor-pointer border-2 border-neutral-500"
-							style={{ background: gradient }}
-							onClick={() => settings.setGradient(gradient as GradientSetting)}
-						/>
-					))}
-				</div>
+		<div className="flex flex-row gap-4 items-center fixed bottom-20 bg-neutral-300 dark:bg-neutral-900/70 p-2 rounded-lg z-10">
+			<div className="flex flex-row gap-1">
+				{gradients.map((gradient: string, index: number) => (
+					<div
+						key={index}
+						className="w-5 h-5 rounded-full bg-white cursor-pointer border-2 border-neutral-500"
+						style={{ background: gradient }}
+						onClick={() => settings.setGradient(gradient as GradientSetting)}
+					/>
+				))}
 			</div>
 
-			<div className="flex flex-col gap-1">
-				<Select
-					value={settings.padding}
-					options={paddingOptions as unknown as string[]}
-					onChange={(option: string) => settings.setPadding(option as PaddingSetting)}
-				/>
-			</div>
+			<Select
+				value={settings.padding}
+				options={paddingOptions as unknown as string[]}
+				onChange={(option: string) => settings.setPadding(option as PaddingSetting)}
+			/>
 
-			<div className="flex flex-col gap-1">
-				<Select
-					value={settings.language}
-					options={['auto', 'none', ...hljs.listLanguages()]}
-					onChange={(option: string) => settings.setLanguage(option as LanguageSetting)}
-				/>
-			</div>
+			<Select
+				value={settings.language}
+				options={['auto', 'none', ...hljs.listLanguages()]}
+				onChange={(option: string) => settings.setLanguage(option as LanguageSetting)}
+			/>
 
-			<div className="flex flex-col justify-end">
-				<Button onClick={takeScreenshot}>
-					<span className="font-semibold">Export</span>
-				</Button>
-			</div>
+			<Button onClick={takeScreenshot}>
+				<span className="font-semibold">Export</span>
+			</Button>
 		</div>
 	);
 });
